c10dc51207a8a2a37470a011a3312737459b8761
Deepak Yadav <123127859+dny5112001@users.noreply.github.com>
2025-01-24 08:35:07 +0530

    Update Footer.tsx
    resolved the image location

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer: React.FC = () => {
       {/* Logo Section */}
       <div className="flex justify-center">
         <Image
-          src="/Images/Logo_footer.png"
+          src="/images/Logo_footer.png"
           alt="EveryX Logo"
           width={50}
           height={50}
@@ -63,7 +63,7 @@ const Footer: React.FC = () => {
       {/* Bottom Logo Section */}
       <div className="flex justify-center mt-20">
         <Image
-          src="/Images/Logo_footer_bottom.png"
+          src="/images/Logo_footer_bottom.png"
           alt="Footer Bottom Logo"
           width={50}
           height={50}
