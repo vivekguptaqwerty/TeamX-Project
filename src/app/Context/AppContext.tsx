eb6e34a379475c528e0886ddec43363d390efe11
= <=>
2025-01-29 00:25:42 +0530

    search bar updated

--- src/app/Context/AppContext.tsx
+++ src/app/Context/AppContext.tsx
@@ -3,40 +3,47 @@
 import React, { createContext, useState, ReactNode } from "react";
 
 interface AppContextProps {
- filter: string;
- setFilter: React.Dispatch<React.SetStateAction<string>>;
- selectedMenu: string;
- setSelectedMenu: React.Dispatch<React.SetStateAction<string>>;
- isLoggedIn: boolean;
- setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  filter: string;
+  setFilter: React.Dispatch<React.SetStateAction<string>>;
+  selectedMenu: string;
+  setSelectedMenu: React.Dispatch<React.SetStateAction<string>>;
+  isLoggedIn: boolean;
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const AppContext = createContext<AppContextProps>({
- filter: "Recommend",
- setFilter: () => {},
- selectedMenu: "Home",
- setSelectedMenu: () => {},
- isLoggedIn: false,
- setIsLoggedIn: () => {},
+  filter: "Recommend",
+  setFilter: () => {},
+  selectedMenu: "Home",
+  setSelectedMenu: () => {},
+  isLoggedIn: false,
+  setIsLoggedIn: () => {},
+  search: "",
+  setSearch: () => {},
 });
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
- const [filter, setFilter] = useState("Reccomend");
- const [selectedMenu, setSelectedMenu] = useState("Home");
- const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [filter, setFilter] = useState("Reccomend");
+  const [selectedMenu, setSelectedMenu] = useState("Home");
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [search, setSearch] = useState("");
 
- return (
-   <AppContext.Provider 
-     value={{ 
-       filter, 
-       setFilter, 
-       selectedMenu, 
-       setSelectedMenu,
-       isLoggedIn,
-       setIsLoggedIn 
-     }}
-   >
-     {children}
-   </AppContext.Provider>
- );
+  return (
+    <AppContext.Provider
+      value={{
+        filter,
+        setFilter,
+        selectedMenu,
+        setSelectedMenu,
+        isLoggedIn,
+        setIsLoggedIn,
+        search,
+        setSearch
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
